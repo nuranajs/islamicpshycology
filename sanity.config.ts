9cd2c5aa85e4e59811173927a711c3941ea4f89d
nuranajs <nurana.co@gmail.com>
2024-12-17 00:47:38 +0700

    change sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,28 +1,12 @@
-'use client'
-
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\admin\[[...tool]]\page.tsx` route
- */
-
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './src/sanity/env'
-import schema from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+// sanity.config.ts
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
 
 export default defineConfig({
+  projectId: '8elj67zb', // Ganti dengan Project ID Anda
+  dataset: 'production',
+  apiVersion: '2024-01-15',
+  title: 'profile',
   basePath: '/admin',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
-  plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
+  plugins: [deskTool()],
 })
